6d58b9687816bd2c49cb87061d559a1b02d1519b
Carlos Henrique Batista da Costa <146479994+Paprikadarlingtoniapurpuraquevedo@users.noreply.github.com>
2025-01-03 22:28:27 +0000

    Versão 1
    Procedimento de conversão do Typescript em JavaScript.

---
+++ ficheiro_segundo/ficheiro_segundo.js
@@ -0,0 +1,10 @@
+var object = "Retrato";
+var face = "Mulher";
+var landscape = "Pinhal";
+var length = 10;
+var width = 5;
+var perimeter = length + length + width + width;
+console.log("Object:" + object);
+console.log("Face:" + face);
+console.log("Landscape:" + landscape);
+console.log("Perimeter:" + perimeter);
---
+++ ficheiro_segundo/ficheiro_segundo.ts
@@ -0,0 +1,10 @@
+var object:string = "Retrato"
+var face:string = "Mulher"
+var landscape: string = "Pinhal"
+var length:number = 10
+var width:number = 5
+var perimeter = length + length + width + width
+console.log("Object:" +object)
+console.log("Face:" +face)
+console.log("Landscape:" +landscape)
+console.log("Perimeter:" +perimeter)
